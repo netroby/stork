beb9249527762d0a4067f10b75dd43a558345d27
Dinesh Israni <disrani@portworx.com>
2018-08-22 16:58:39 -0700

    Keep a track of captured resources to avoid duplicates
    Non-namespaces items will show up everytime when iterating through
    resources and multiple namespaces

--- pkg/migration/controllers/migration.go
+++ pkg/migration/controllers/migration.go
@@ -17,6 +17,7 @@ import (
 	"github.com/operator-framework/operator-sdk/pkg/sdk"
 	"github.com/portworx/sched-ops/k8s"
 	"github.com/sirupsen/logrus"
+	"k8s.io/api/core/v1"
 	apiextensionsv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	apiextensionsclient "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 	"k8s.io/apimachinery/pkg/api/errors"
@@ -26,7 +27,9 @@ import (
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 )
@@ -216,7 +219,14 @@ func resourceToBeMigrated(migration *stork_crd.Migration, resource metav1.APIRes
 	}
 }
 
-func objectToBeMigrated(object runtime.Unstructured) (bool, error) {
+func objectToBeMigrated(resourceMap map[types.UID]bool, object runtime.Unstructured) (bool, error) {
+	metadata, err := meta.Accessor(object)
+	if err != nil {
+		return false, err
+	}
+	if _, ok := resourceMap[metadata.GetUID()]; ok {
+		return false, nil
+	}
 	objectType, err := meta.TypeAccessor(object)
 	if err != nil {
 		return false, err
@@ -254,6 +264,7 @@ func (m *MigrationController) migrateResources(migration *stork_crd.Migration) e
 			continue
 		}
 
+		resourceMap := make(map[types.UID]bool)
 		for _, resource := range group.APIResources {
 			if !resourceToBeMigrated(migration, resource) {
 				continue
@@ -280,7 +291,7 @@ func (m *MigrationController) migrateResources(migration *stork_crd.Migration) e
 						return fmt.Errorf("Error casting object: %v", o)
 					}
 
-					migrate, err := objectToBeMigrated(runtimeObject)
+					migrate, err := objectToBeMigrated(resourceMap, runtimeObject)
 					if err != nil {
 						return fmt.Errorf("Error processing object %v: %v", runtimeObject, err)
 					}
@@ -305,6 +316,7 @@ func (m *MigrationController) migrateResources(migration *stork_crd.Migration) e
 					resourceInfo.Version = groupVersion.Version
 					resourceInfos = append(resourceInfos, resourceInfo)
 					allObjects = append(allObjects, runtimeObject)
+					resourceMap[metadata.GetUID()] = true
 				}
 			}
 		}
@@ -339,7 +351,9 @@ func (m *MigrationController) prepareResources(
 ) error {
 	for _, o := range objects {
 		content := o.UnstructuredContent()
+		// Status shouldn't be migrated between clusters
 		delete(content, "status")
+
 		switch o.GetObjectKind().GroupVersionKind().Kind {
 		case "PersistentVolume":
 			updatedObject, err := m.preparePVResource(migration, o)
@@ -461,6 +475,28 @@ func (m *MigrationController) applyResources(
 		return err
 	}
 
+	// First make sure all the namespaces are created on the
+	// remote cluster
+	client, err := kubernetes.NewForConfig(remoteConfig)
+	if err != nil {
+		return err
+	}
+	for _, ns := range migration.Spec.Namespaces {
+		namespace, err := k8s.Instance().GetNamespace(ns)
+		if err != nil {
+			return err
+		}
+		_, err = client.CoreV1().Namespaces().Create(&v1.Namespace{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:   namespace.Name,
+				Labels: namespace.Labels,
+			},
+		})
+		if err != nil && apierrors.IsAlreadyExists(err) {
+			return err
+		}
+	}
+
 	remoteDynamicClientPool := dynamic.NewDynamicClientPool(remoteConfig)
 	for _, o := range objects {
 		dynamicClient, err := remoteDynamicClientPool.ClientForGroupVersionKind(o.GetObjectKind().GroupVersionKind())
@@ -490,26 +526,28 @@ func (m *MigrationController) applyResources(
 			switch objectType.GetKind() {
 			// Don't want to delete the Volume resources
 			case "PersistentVolumeClaim", "PersistentVolume":
-				break
+				err = nil
 			default:
 				err = client.Delete(metadata.GetName(), &metav1.DeleteOptions{})
-				_, err = client.Create(unstructured)
-				if err != nil {
-					m.updateResourceStatus(
-						migration,
-						o,
-						stork_crd.MigrationStatusFailed,
-						fmt.Sprintf("Failed to migrate resource: %v", err))
-					continue
+				if err == nil {
+					_, err = client.Create(unstructured)
 				}
 			}
 
 		}
-		m.updateResourceStatus(
-			migration,
-			o,
-			stork_crd.MigrationStatusSuccessful,
-			"Resource migrated successfully")
+		if err != nil {
+			m.updateResourceStatus(
+				migration,
+				o,
+				stork_crd.MigrationStatusFailed,
+				fmt.Sprintf("Error applying resource: %v", err))
+		} else {
+			m.updateResourceStatus(
+				migration,
+				o,
+				stork_crd.MigrationStatusSuccessful,
+				"Resource migrated successfully")
+		}
 	}
 	return nil
 }
